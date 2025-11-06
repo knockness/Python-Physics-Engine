233ba0b958f632f3dfe1b27bfc0ab30ad71921f7
Knockness <virajwatal@gmail.com>
2025-11-06 14:09:52 -0300

    Added constructors
    used classes for constructors like ball.position.x and such

--- Ascii_Physics.py
+++ Ascii_Physics.py
@@ -1,6 +1,7 @@
+import time
 import os
 
-#Variables
+# Scene Variables
 sceneX = 50
 sceneY = 25
 
@@ -8,25 +9,45 @@ sceneArray = [
 
 ]
 
-# clear terminal screen def
+# Ball Variables
+class Position:
+    def __init__(self, x, y):
+        self.x = x  
+        self.y = y
+        
+class Ball:
+    def __init__(self):
+        self.position = Position(0, 0) # starting positon
+
+prevPositon = []
+
+ball = Ball()
+# Terminal Screen Control
+    # clear terminal screen def
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-# draw blank screen def
+    # Print Written Scene
+def printScene():
+    clear()
+    for sublist in sceneArray:
+        print(" ".join(map(str, sublist)))
+
+# Scene Writing
+    # draw blank screen def
 def drawBlank():
     clear()
     for i in range(sceneY):
         sceneRow = ['`']*sceneX
         sceneArray.append(sceneRow)
-    
+
 drawBlank()
 
-for sublist in sceneArray:
-    print(" ".join(map(str, sublist)))
+# Runtime
+while True:
+#Physics 
+    sceneArray[ball.position.y][ball.position.x] = '@'
 
-# while True:
-#     #Physics 
-    
-#     #Ascii Rendering
-#     clear()
-#     break
+#Ascii Rendering
+    printScene()
+    time.sleep(0.1)
